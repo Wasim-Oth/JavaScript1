ccfe93d17203950a6dee5166f2e5b6fff38bc3a9
Wasim Othman <wasim1991othman@gmail.com>
2020-07-15 11:20:57 +0200

    added hello world in 10 languages to hello-world.js

--- Week1/hello-world.js
+++ Week1/hello-world.js
@@ -1,3 +1,11 @@
 'use strict'
-var msg = 'hello world';
-console.log(msg);
+console.log("hello world");
+console.log("قول معكرونة");
+console.log("你好，世界");
+console.log("Olá Mundo");
+console.log("Ciao mondo");
+console.log("こんにちは世界");
+console.log("Selam Dünya");
+console.log("Привет мир");
+console.log("Hallo Wereld");
+console.log("Hallo Welt");
