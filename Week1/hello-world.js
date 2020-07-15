ddff38482ca5e9f826c808650bfab8506879efe6
Wasim Othman <wasim1991othman@gmail.com>
2020-07-15 12:02:44 +0200

    added languages commits

--- Week1/hello-world.js
+++ Week1/hello-world.js
@@ -1,11 +1,20 @@
 'use strict'
 console.log("hello world");
+// arabic
 console.log("قول معكرونة");
+// chinese
 console.log("你好，世界");
-console.log("Olá Mundo");
+// spanish
+console.log("Hola mundo");
+// italian
 console.log("Ciao mondo");
+// japanese
 console.log("こんにちは世界");
+// turkish
 console.log("Selam Dünya");
+// russian
 console.log("Привет мир");
+// dutchiiii
 console.log("Hallo Wereld");
+// duits
 console.log("Hallo Welt");
