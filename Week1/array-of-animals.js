ad18d7fb796cde8c603da09270bab49735a390b6
Wasim Othman <wasim1991othman@gmail.com>
2020-07-16 00:51:49 +0200

    added file array-of-animals.js

---
+++ Week1/array-of-animals.js
@@ -0,0 +1,22 @@
+'use strict'
+let drinks = [];
+console.log('[]')
+console.log(drinks);
+
+let animals = ['wolf', 'cat', 'rabbit'];
+console.log(animals);
+
+animals.push('Piglet');
+console.log(animals);
+
+
+
+
+
+
+
+
+
+
+
+
