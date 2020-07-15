aaf91bde2b7cfc22f3ac862057856d224ef2da00
Wasim Othman <wasim1991othman@gmail.com>
2020-07-15 18:34:15 +0200

    comments deleted

--- Week1/log-the-number.js
+++ Week1/log-the-number.js
@@ -1,9 +1,9 @@
 'use strict'
-// let numberX;
-// console.log('undefined');
-// console.log(numberX);
+let numberX;
+console.log('undefined');
+console.log(numberX);
 
-let numberX= 5;
+numberX= 5;
 console.log('5');
 console.log(numberX);
 
