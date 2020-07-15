32689a225a4894ea67b1c49c3ed4416f58c9b2e7
Wasim Othman <wasim1991othman@gmail.com>
2020-07-15 21:12:23 +0200

    added file round-number.js

---
+++ Week1/round-number.js
@@ -0,0 +1,18 @@
+let z = 7.25;
+console.log(z);
+
+let a = Math.round(z);
+console.log(a);
+
+if (z > a) {
+    let b = z;
+    console.log(b);
+    }
+    else{
+        let b = z;
+        console.log(z);
+}
+    
+
+
+
