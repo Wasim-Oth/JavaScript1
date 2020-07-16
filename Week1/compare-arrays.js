40bf52ac57146e16e202b2aaa382c4fd71aac7ec
Wasim Othman <wasim1991othman@gmail.com>
2020-07-16 03:38:27 +0200

    added to compare-arrays.js a conditional statement that checks if both var are of equal length

--- Week1/compare-arrays.js
+++ Week1/compare-arrays.js
@@ -4,3 +4,10 @@ let drinks = ['beer', 'cola', 'water', 'vodka', 'juice', 'wisky', 'soda'];
 
 console.log('the length of the array is:' +' ' + food.length);
 console.log('the length of the array is:' +' ' + drinks.length);
+
+if (food.length == drinks.length){
+console.log('They are the same!');
+}
+else{
+    console.log('Two different sizes');
+}
