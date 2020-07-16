626d986c256edf9c95f036a677bf362cb0920169
Wasim Othman <wasim1991othman@gmail.com>
2020-07-16 03:31:53 +0200

    added compare-arrays.js

---
+++ Week1/compare-arrays.js
@@ -0,0 +1,6 @@
+'use strict'
+let food = ['pizza', 'shorma', 'kabab', 'salada' ];
+let drinks = ['beer', 'cola', 'water', 'vodka', 'juice', 'wisky', 'soda'];
+
+console.log('the length of the array is:' +' ' + food.length);
+console.log('the length of the array is:' +' ' + drinks.length);
