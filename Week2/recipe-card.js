4aa5d2179b8502b3141fd3a5022a002f61e7b085
Wasim Othman <wasim1991othman@gmail.com>
2020-07-21 00:34:37 +0200

    added recipe-card.js

---
+++ Week2/recipe-card.js
@@ -0,0 +1,9 @@
+'use stict'
+
+let food= {};
+food = {MealName: 'maklobe' ,
+        servings: 2 ,
+        ingredients: ['half kilo Aubergine', '1 glas rice', 'half kilo chopped meat', '1 magi' ] 
+       };
+
+       console.log(food)
