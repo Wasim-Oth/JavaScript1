2ddfbe46f327cead6e8f1ab36f290ed88134ca9a
Wasim Othman <wasim1991othman@gmail.com>
2020-07-23 01:11:26 +0200

    added reading-list.js & added project-grade-calculator.js

---
+++ Week2/project-grade-calculator.js
@@ -0,0 +1,41 @@
+'use strict'
+
+let grade= 'd' ;
+switch (grade){
+    case 'a':
+        console.log('You got a A (90% - 100%)');
+        break;
+    
+    case 'b':
+        console.log('You got a B (80% - 89%)');
+        break;
+    
+    case 'c':
+        console.log('You got a C (70% - 79%)');
+        break;
+    
+    case 'd':
+        console.log('You got a D (60% - 69%)');
+        break;
+    
+    case 'e':
+        console.log('You got a E (50% - 59%)');
+        break;
+    
+    case 'f':
+        console.log('Grade F (0% - 49%)');
+    
+    console.log(grade)
+    
+}
+
+
+
+
+
+
+
+
+
+
+// project-grade-calculator.js
---
+++ Week2/reading-list.js
@@ -0,0 +1,34 @@
+'use strict';
+
+       // is it not possible to use (.slice()) after books.[i]? i tried and it didn't work. i couldn't find another way to to hide the alreadyRead proporty!  
+
+let books = [
+    {
+        title: ' the subtle art of not giving a fuck',
+        author: 'Mark Manson',
+        alreadyRead: true,
+    },
+    {
+        title: 'war and peace',
+        author: 'Leo Tolstoy',
+        alreadyRead: false,
+    },
+    {
+        title: 'the alchemist',
+        author: 'Paulo Coelho',
+        alreadyRead: false,
+    }
+];
+
+
+
+for (let i=0; i< books.length; i++){
+    console.log (books[i]);
+    if (books[i].alreadyRead == false){
+        console.log(' I still need to read ' +  books[i].title)
+    }
+    else{
+        console.log('I already read' + books[i].title)
+    }
+};
+    
