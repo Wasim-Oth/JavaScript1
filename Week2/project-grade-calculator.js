faf44500bf191846d01235906acb04af6564ef0a
Wasim Othman <wasim1991othman@gmail.com>
2020-07-25 18:54:32 +0200

    changed project-grade-calculator, commented switch statement and used if statment

--- Week2/project-grade-calculator.js
+++ Week2/project-grade-calculator.js
@@ -1,33 +1,62 @@
 'use strict'
 
-let grade= 'd' ;
-switch (grade){
-    case 'a':
-        console.log('You got a A (90% - 100%)');
-        break;
+        // after using switch i Commented it and used if. which one is better?
+let grade= 55;
+let scoor = "";
+    if (grade >= 90 ){
+        scoor= 'You got a A (90% - 100%)'
+    }
     
-    case 'b':
-        console.log('You got a B (80% - 89%)');
-        break;
+    else if (grade >= 80 ){
+        scoor= 'You got a B (80% - 89%)'
+    }
+        
+    else if (grade >= 70 ){
+        scoor= 'You got a C (70% - 79%)'
+    }
     
-    case 'c':
-        console.log('You got a C (70% - 79%)');
-        break;
+    else if (grade >= 60 ){
+        scoor= 'You got a D (60% - 69%)'
+    }   
+    else if (grade >= 50 ){
+        scoor= 'You got a E (50% - 59%)'
+    }
+    else {
+        scoor= 'You got a F (0% - 49%)'
+    };
+
+    console.log(scoor);
+
+
+
+// let grade= 'd' ;
+// switch (grade){
+//     case 'a':
+//         console.log('You got a A (90% - 100%)');
+//         break;
     
-    case 'd':
-        console.log('You got a D (60% - 69%)');
-        break;
+//     case 'b':
+//         console.log('You got a B (80% - 89%)');
+//         break;
     
-    case 'e':
-        console.log('You got a E (50% - 59%)');
-        break;
+//     case 'c':
+//         console.log('You got a C (70% - 79%)');
+//         break;
     
-    case 'f':
-        console.log('Grade F (0% - 49%)');
+//     case 'd':
+//         console.log('You got a D (60% - 69%)');
+//         break;
     
-    console.log(grade)
+//     case 'e':
+//         console.log('You got a E (50% - 59%)');
+//         break;
     
-}
+//     case 'f':
+//         console.log('Grade F (0% - 49%)');
+    
+//     console.log(grade)
+    
+// }
 
 
 
