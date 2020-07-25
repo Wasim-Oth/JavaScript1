3b35fa1840214a2fbe8c31d23d825cb4d166d10e
Wasim Othman <wasim1991othman@gmail.com>
2020-07-25 23:00:05 +0200

    project-grade-calculator.js changed, added percentage variable

--- Week2/project-grade-calculator.js
+++ Week2/project-grade-calculator.js
@@ -1,70 +1,28 @@
 'use strict'
 
-        // after using switch i Commented it and used if. which one is better?
-let grade= 55;
+let grade= 93;
 let scoor = "";
+var percentage = Number(grade/100).toLocaleString(undefined,{style: 'percent'}); 
     if (grade >= 90 ){
-        scoor= 'You got a A (90% - 100%)'
+        scoor= 'You got a A ' + (percentage);
     }
     
     else if (grade >= 80 ){
-        scoor= 'You got a B (80% - 89%)'
+        scoor= 'You got a B ' + (percentage);
     }
         
     else if (grade >= 70 ){
-        scoor= 'You got a C (70% - 79%)'
+        scoor= 'You got a C ' + (percentage);
     }
     
     else if (grade >= 60 ){
-        scoor= 'You got a D (60% - 69%)'
+        scoor= 'You got a D ' + (percentage);
     }   
     else if (grade >= 50 ){
-        scoor= 'You got a E (50% - 59%)'
+        scoor= 'You got a E ' + (percentage);
     }
     else {
-        scoor= 'You got a F (0% - 49%)'
+        scoor= 'You got a F ' + (percentage);
     };
 
     console.log(scoor);
-
-
-
-// let grade= 'd' ;
-// switch (grade){
-//     case 'a':
-//         console.log('You got a A (90% - 100%)');
-//         break;
-    
-//     case 'b':
-//         console.log('You got a B (80% - 89%)');
-//         break;
-    
-//     case 'c':
-//         console.log('You got a C (70% - 79%)');
-//         break;
-    
-//     case 'd':
-//         console.log('You got a D (60% - 69%)');
-//         break;
-    
-//     case 'e':
-//         console.log('You got a E (50% - 59%)');
-//         break;
-    
-//     case 'f':
-//         console.log('Grade F (0% - 49%)');
-    
-//     console.log(grade)
-    
-// }
-
-
-
-
-
-
-
-
-
-
-// project-grade-calculator.js
