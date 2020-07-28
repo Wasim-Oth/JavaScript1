bd721ec683eb4c30901f773a275bce02bb730b20
Wasim Othman <wasim1991othman@gmail.com>
2020-07-28 18:19:23 +0200

    added dog-years.js

---
+++ Week3/dog-years.js
@@ -0,0 +1,7 @@
+'use strict'
+
+function calculateDogAge(num){
+    let dogAge=  `this is your dog age ${num * 7} years old in dog years! `;
+    return dogAge;
+}
+console.log (calculateDogAge(2));
