d2e8759f24d55b1002c1bd8c056772874711d992
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-07-28 14:49:09 +0200

    Update type-checker.js
    added  function (checkIfSameType). instead of (if) statment.

--- Week1/type-checker.js
+++ Week1/type-checker.js
@@ -19,47 +19,62 @@ console.log(typeof(lastName));
 console.log(typeof(info));
 console.log(typeof(moreInfo));
 
-
-if (typeof(firstName) == typeof(lastName)){
-console.log('same type');
+function checkIfSameType (val1, val2,  val3){
+    if (typeof (val1) == typeof(val3)){
+        console.log(typeof val1, val2, typeof val3  + ' are same type');
+    } 
+    else {
+        console.log(typeof val1, val2, typeof val3 + ' are not the same type')
+    };
 }
-else {
-    console.log('not the same type')
-};
-
-if (typeof(firstName) == typeof(info)){
-    console.log('same type');
-    }
-else {
-        console.log('not the same type')
-     };
-
-if (typeof(firstName) == typeof(moreInfo)){
-    console.log('same type');
-        }
-else {
-        console.log('not the same type')
-     };
+checkIfSameType(firstName, 'and', lastName);
+checkIfSameType(firstName, 'and', info);
+checkIfSameType(firstName, 'and', moreInfo);
+checkIfSameType(lastName, 'and', info);
+checkIfSameType(lastName, 'and', moreInfo);
+checkIfSameType(info    , 'and', moreInfo);
 
 
-if (typeof(lastName) == typeof(info)){
-     console.log('same type');
-     }
-else {
-      console.log('not the same type')
-     };
+// if (typeof(firstName) == typeof(lastName)){
+// console.log('same type');
+// }
+// else {
+//     console.log('not the same type')
+// };
 
-if (typeof(lastName) == typeof(moreInfo)){
-        console.log('same type');
-        }
-else {
-       console.log('not the same type')
-     };    
+// if (typeof(firstName) == typeof(info)){
+//     console.log('same type');
+//     }
+// else {
+//         console.log('not the same type')
+//      };
 
-if (typeof(info) == typeof(moreInfo)){
-        console.log('same type');
-     }
-else {
-       console.log('not the same type')
-     };          
+// if (typeof(firstName) == typeof(moreInfo)){
+//     console.log('same type');
+//         }
+// else {
+//         console.log('not the same type')
+//      };
+
+
+// if (typeof(lastName) == typeof(info)){
+//      console.log('same type');
+//      }
+// else {
+//       console.log('not the same type')
+//      };
+
+// if (typeof(lastName) == typeof(moreInfo)){
+//         console.log('same type');
+//         }
+// else {
+//        console.log('not the same type')
+//      };    
+
+// if (typeof(info) == typeof(moreInfo)){
+//         console.log('same type');
+//      }
+// else {
+//        console.log('not the same type')
+//      };          
 
