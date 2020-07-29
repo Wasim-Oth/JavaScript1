c85f434d846841ede4308f35ba23933c7cd51475
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-07-29 11:10:15 +0200

    Update who-wants-drink.js
    i think this is a bit better than before. but i still have a question: how can i push each element twice without doing this drinkTray.push(drinkTypes[i]); twice, cus i don't think  a programmer will do it like this!

--- Week2/who-wants-drink.js
+++ Week2/who-wants-drink.js
@@ -1,22 +1,28 @@
 'use strict';
 
-
-                    //  i dont think this is the right way! and i feel frustrated cus i cant't do this easy tasks!
-
 let drinkTray= [];
-const drinkTypes = ['cola', 'lemonade', 'water'];
-
-
+const drinkTypes = ['cola', 'lemonade', 'water' ];
 
 let rows = drinkTypes.length;
 for (let i=0; i < rows; i++){
     drinkTray.push(drinkTypes[i]);
+    drinkTray.push(drinkTypes[i]);
+    if (i === rows-1){
+        drinkTray.pop();
+    }
+  
+console.log(`Hey guys, I brought a ${drinkTray}`);
+
+
+
+// for (let i=0; i < rows; i++){
+//     drinkTray.push(drinkTypes[i]);
     
-    if (drinkTypes[i]== 'cola'){
-        drinkTray.push('cola');
-    }
-    if (drinkTypes[i]== 'lemonade'){
-        drinkTray.push('lemonade');
-    }
-}
-console.log('Hey guys, I brought a' +' '+ drinkTray);
+//     if (drinkTypes[i]== 'cola'){
+//         drinkTray.push('cola');
+//     }
+//     if (drinkTypes[i]== 'lemonade'){
+//         drinkTray.push('lemonade');
+//     }
+// }
+// console.log('Hey guys, I brought a' +' '+ drinkTray);
