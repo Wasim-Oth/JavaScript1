2002074f05760858ecd0d3333564312ef105252c
Wasim-Oth <57799081+Wasim-Oth@users.noreply.github.com>
2020-07-29 11:43:42 +0200

    Update project-grade-calculator.js
    added function getScoor() takes one parameter (grade). deleted 2 variables scoor and grade. added return instead of the variable scoor .

--- Week2/project-grade-calculator.js
+++ Week2/project-grade-calculator.js
@@ -1,28 +1,55 @@
 'use strict'
 
-let grade= 93;
-let scoor = "";    //content the grade with percentage
+function getScoor(grade){
 let percentage = Number(grade/100).toLocaleString(undefined,{style: 'percent'});   // to change the number to percentage
     if (grade >= 90 ){
-        scoor= 'You got a A ' + (percentage);
+        return 'You got a A ' + (percentage);
     }
     
     else if (grade >= 80 ){
-        scoor= 'You got a B ' + (percentage);
+        return 'You got a B ' + (percentage);
     }
         
     else if (grade >= 70 ){
-        scoor= 'You got a C ' + (percentage);
+        return 'You got a C ' + (percentage);
     }
     
     else if (grade >= 60 ){
-        scoor= 'You got a D ' + (percentage);
+        return 'You got a D ' + (percentage);
     }   
     else if (grade >= 50 ){
-        scoor= 'You got a E ' + (percentage);
+        return 'You got a E ' + (percentage);
     }
     else {
-        scoor= 'You got a F ' + (percentage);
+        return 'You got a F ' + (percentage);
     };
+};
 
-    console.log(scoor);
+    console.log(getScoor(67));
+
+// let grade= 93;
+// let scoor = "";    //content the grade with percentage
+// let percentage = Number(grade/100).toLocaleString(undefined,{style: 'percent'});   // to change the number to percentage
+//     if (grade >= 90 ){
+//         scoor= 'You got a A ' + (percentage);
+//     }
+    
+//     else if (grade >= 80 ){
+//         scoor= 'You got a B ' + (percentage);
+//     }
+        
+//     else if (grade >= 70 ){
+//         scoor= 'You got a C ' + (percentage);
+//     }
+    
+//     else if (grade >= 60 ){
+//         scoor= 'You got a D ' + (percentage);
+//     }   
+//     else if (grade >= 50 ){
+//         scoor= 'You got a E ' + (percentage);
+//     }
+//     else {
+//         scoor= 'You got a F ' + (percentage);
+//     };
+
+//     console.log(scoor);
